4e61ca11aa7c5e75d3b9d656ae4b6ccb3874aa53
Caique Carvalho da Silva <caique.carvalho3033@gmail.com>
2021-08-12 19:02:39 -0300

    redo the project

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.querySelector("#btn").onclick = function(){
-    const input = document.querySelector("#input");
-    const reversed_input = document.querySelector("#reversed_input");
-    const text_field = document.querySelector("#txtfield").value;
+document.querySelector("#button").onclick = function(){
+    const input = document.querySelector(".word");
+    const reversed_input = document.querySelector(".inverted-word");
+    const text_field = document.querySelector("#text-field").value;
     var inputArr = Array.from((String)(text_field).trim());
 
     if((String)(text_field).trim() != ""){
@@ -14,4 +14,11 @@ document.querySelector("#btn").onclick = function(){
         input.innerText = (String)(text_field).charAt(0).toUpperCase() + (String)(text_field).slice(1);
         reversed_input.innerText = inputArr.join("").charAt(0).toUpperCase() + (String)(inputArr.join("")).slice(1);
     }
+}
+
+// Randow word generator
+onload = function() {
+    const word = document.querySelector(".word");
+    const invertedWord = document.querySelector(".inverted-word");
+    
 }
